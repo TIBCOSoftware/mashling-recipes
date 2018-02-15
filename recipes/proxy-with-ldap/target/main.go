dee8d2eb868d0089e6117d6f5de99aa3f1ceb42f
Andrew Snodgrass <git@pointlander.info>
2018-02-15 12:41:33 -0700

    Added LDAP recipe

---
+++ recipes/proxy-with-ldap/target/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println(r.RequestURI)
+		log.Println(string(body))
+	}
+	http.HandleFunc("/a", handler)
+	http.HandleFunc("/b", handler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
