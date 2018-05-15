ee46ca9d201ba45ed380c43dbc1673689304e698
LakshmiMekala <lmekala@tibco.com>
2018-05-15 20:41:46 +0530

    Feature v2 model cicd (#68)
    * adding sanity files to support v2
    
    * Adding branch for builds
    
    * travis updates
    
    * script enhancements
    
    * update travis file
    
    * test script enhancements
    
    * Add FCM & Node related folders to gitignore

--- recipes/KafkaTrigger-To-KafkaPublisher/KafkaTrigger-To-KafkaPublisher.sh
+++ recipes/KafkaTrigger-To-KafkaPublisher/KafkaTrigger-To-KafkaPublisher.sh
@@ -25,7 +25,7 @@ function testcase1 {
     popd
 
     #executing the gateway binary
-    ./kafkatrigger-to-kafkapublisher &
+    mashling-gateway -c kafkatrigger-to-kafkapublisher.json > /tmp/kafka1.log &
     pId4=$!
     sleep 20
 
