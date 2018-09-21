1cb29a1b02e83b5e121477b63d234e0572c04fb1
nareshkumarthota <nthota@tibco.com>
2018-09-21 14:45:50 +0530

    first cut files for grpc streaming

--- recipes/grpc-to-grpc-gateway/main.go
+++ recipes/grpc-to-grpc-gateway/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"strconv"
@@ -24,6 +25,9 @@ type ServerStrct struct {
 
 var petMapArr = make(map[int32]pb.Pet)
 var userMapArr = make(map[string]pb.User)
+
+var recUserArrClient = make([]pb.User, 0)
+var recUserArrServer = make([]pb.User, 0)
 
 var petArr = []pb.Pet{
 	{
@@ -57,6 +61,48 @@ var userArr = []pb.User{
 		Username: "user4",
 		Email:    "email4",
 		Phone:    "phone4",
+	},
+}
+
+var sendUserDataClient = []pb.User{
+	{
+		Id:       22,
+		Username: "user22c",
+		Email:    "email22c",
+		Phone:    "phone22c",
+	},
+	{
+		Id:       23,
+		Username: "user23c",
+		Email:    "email23c",
+		Phone:    "phone23c",
+	},
+	{
+		Id:       24,
+		Username: "user24c",
+		Email:    "email24c",
+		Phone:    "phone24c",
+	},
+}
+
+var sendUserDataServer = []pb.User{
+	{
+		Id:       32,
+		Username: "user32s",
+		Email:    "email32s",
+		Phone:    "phone32s",
+	},
+	{
+		Id:       33,
+		Username: "user33s",
+		Email:    "email33s",
+		Phone:    "phone33s",
+	},
+	{
+		Id:       34,
+		Username: "user34s",
+		Email:    "email34s",
+		Phone:    "phone34s",
 	},
 }
 
@@ -109,6 +155,12 @@ func callClient(port *string, option *string, id int, name string) {
 		PetById(client, id)
 	case "user":
 		UserByName(client, name)
+	case "userstream":
+		ListUsers(client)
+	case "storeusers":
+		StoreUsers(client)
+	case "bulkusers":
+		BulkUsers(client)
 	}
 }
 
@@ -128,6 +180,71 @@ func PetById(client pb.PetStoreServiceClient, id int) {
 		log.Fatal(err)
 	}
 	fmt.Println("res :", res)
+}
+
+func ListUsers(client pb.PetStoreServiceClient) {
+	stream, err := client.ListUsers(context.Background(), &pb.EmptyReq{Msg: "hi gimme list of users"})
+	if err != nil {
+		fmt.Println("erorr occured in ListUsers", err)
+	}
+	for {
+		users, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("erorr occured while recieving ", err)
+		}
+		fmt.Println("streamed data:", users)
+	}
+}
+
+func StoreUsers(client pb.PetStoreServiceClient) {
+	stream, err := client.StoreUsers(context.Background())
+	if err != nil {
+		fmt.Println("erorr occured in StoreUsers", err)
+	}
+	for _, user := range sendUserDataClient {
+		if err := stream.Send(&user); err != nil {
+			fmt.Println("error while sending user", user, err)
+		}
+	}
+	reply, err := stream.CloseAndRecv()
+	if err != nil {
+		fmt.Println("erorr occured in StoreUsers response", err)
+	}
+	fmt.Println("response received from server:", reply)
+}
+
+func BulkUsers(client pb.PetStoreServiceClient) {
+	stream, err := client.BulkUsers(context.Background())
+	if err != nil {
+		fmt.Println("error in streaming of bulk users", err)
+	}
+	waitc := make(chan struct{})
+	go func() {
+		for {
+			user, err := stream.Recv()
+			if err == io.EOF {
+				// read done.
+				fmt.Println("@@@@@@@@@@received total@@@@@@@@@", len(recUserArrClient), "[", recUserArrClient, "]")
+				recUserArrClient = nil
+				close(waitc)
+				return
+			}
+			if err != nil {
+				log.Fatalf("Failed to receive a user : %v", err)
+			}
+			recUserArrClient = append(recUserArrClient, *user)
+		}
+	}()
+	for _, user := range sendUserDataClient {
+		if err := stream.Send(&user); err != nil {
+			log.Fatalf("Failed to send a user: %v", err)
+		}
+	}
+	stream.CloseSend()
+	<-waitc
 }
 
 func callServer() {
@@ -172,4 +289,60 @@ func (t *ServerStrct) UserByName(ctx context.Context, req *pb.UserByNameRequest)
 
 	return nil, errors.New("User not found")
 
+}
+
+func (t *ServerStrct) ListUsers(req *pb.EmptyReq, sReq pb.PetStoreService_ListUsersServer) error {
+	fmt.Println("server ListUsers method called")
+	fmt.Println("request recieved from client ", req)
+	for _, user := range userMapArr {
+		sReq.Send(&user)
+	}
+	return nil
+}
+func (t *ServerStrct) StoreUsers(cReq pb.PetStoreService_StoreUsersServer) error {
+	fmt.Println("server StoreUsers method called")
+	var i = 0
+	for {
+		userData, err := cReq.Recv()
+		if userData != nil {
+			i++
+			userMapArr[userData.GetUsername()] = *userData
+		}
+		if err == io.EOF {
+			return cReq.SendAndClose(&pb.EmptyRes{Msg: "recieved total users:" + strconv.Itoa(i)})
+		}
+		if err != nil {
+			return err
+		}
+	}
+}
+func (t *ServerStrct) BulkUsers(bReq pb.PetStoreService_BulkUsersServer) error {
+	fmt.Println("server BulkUsers method called")
+	waits := make(chan struct{})
+	go func() {
+		for {
+			userData, err := bReq.Recv()
+			if userData != nil {
+				recUserArrServer = append(recUserArrServer, *userData)
+			}
+			if err == io.EOF {
+				fmt.Println("@@@@@@ total records received @@@@@", len(recUserArrServer), "data:", recUserArrServer)
+				recUserArrServer = nil
+				close(waits)
+				return
+			}
+			if err != nil {
+				fmt.Println("error occured while receiving", err)
+				return
+			}
+		}
+	}()
+	for _, user := range sendUserDataServer {
+		err := bReq.Send(&user)
+		if err != nil {
+			fmt.Println("error occured in sending data", err)
+		}
+	}
+	<-waits
+	return nil
 }
